a4eb4dfdfa13936d480e0b8227a5d7e5005a0570
Duran Israel <98367725+DuranIsrael@users.noreply.github.com>
2022-07-06 19:52:05 -0500

    adding some explaination to the code

--- web-scraper.py
+++ web-scraper.py
@@ -9,20 +9,22 @@ url = "https://pogotrainer.club//"
 results = requests.get(url)
 results.raise_for_status() #this will print in error should there be an issue with the website
 doc = BeautifulSoup(results.text, "html.parser") #taking the results of the url and puttiin it in text format
-trainers = doc.find('article').find_all('div', class_="media") #specifiying the class that i want soup to find with the particular class name.
+trainers = doc.find('article').find_all('div', class_="media") #specifiying the class that I want soup to find with the particular class name.
 
-trainer_names=[]
+#these empty list will contain the information we mine from the website.
+trainer_names=[] 
 trainer_codes=[]
 
+#this "for loop" will iterate through the "trainers" variable that contains the information from the webiste and grab the desired information
 for trainer in trainers:
-    trainer_name = trainer.find('h4', class_="media-heading").text
+    trainer_name = trainer.find('h4', class_="media-heading").text 
     trainer_code = trainer.find('a', class_="TCLink").text
     print(trainer_name, trainer_code)
 
     trainer_names.append(trainer_name)
     trainer_codes.append(trainer_code)
     
-    
+#the code below will generate a generic exel file that will hold our information mined from the website (panda)   
 mylist = pd.DataFrame({'Trainer Names:':trainer_names, 'Trainer Numbers:':trainer_codes})
 mylist.to_csv("TrainerConnect.csv")
 
